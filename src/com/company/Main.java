efd83a5b39eba5401ab4323a25c1aa272ab34b22
Abrams <dima.abramchik@gmail.com>
2021-05-21 20:17:10 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long result = 1l;
+        long number = 1l;
+        do {
+            result = result * number;
+            ++number;
+        } while (number >= 1 && number < 25);
+        System.out.println("Произведение чисел от 1 до 25 = " + result);
+    }
+}
